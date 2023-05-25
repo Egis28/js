f063cafaefdeaad5414f82672cb7dcc445f66e3a
Moksleivis <Moksleivis@kitm.local>
2023-05-25 12:35:37 +0300

    laba diena

---
+++ first-react-project/src/App.js
@@ -0,0 +1,15 @@
+import BooksList from "./components/books/BooksList";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+<Header/>
+<BooksList/>
+    </div>
+      
+  
+  );
+}
+
+export default App;
